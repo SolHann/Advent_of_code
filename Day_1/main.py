363d207af5a720513eb8302e0b733e23e8157c6d
SolHann <supersolhann@gmail.com>
2022-12-01 18:48:50 +0000

    Adds ability to find three largest 'elves'

--- Day_1/main.py
+++ Day_1/main.py
@@ -1,20 +1,33 @@
-input1 = open('input.txt')
-
-
-def max_of_sum():
-    ints = input1.read().split("\n")
+# Returns a list of the sums of ints split by an empty line(/n/n)
+def sum_groups(input):
+    ints = input.read().split("\n")
 
     sum = 0
     sum_l = []
     for x in ints:
-        if x == '':
+        if x == '':  # Since we have already split by /n x = '' indicates an empty line
             sum_l.append(sum)
             sum = 0
             continue
         sum += int(x)
-    return max(sum_l)
+    return sum_l
 
 
-# Press the green button in the gutter to run the script.
+# Return the three largest ints in a list
+def three_largest(list):
+    big_three = []
+    for i in range(3):
+        x = max(list)
+        big_three.append(x)
+        list.remove(x)
+
+    return big_three
+
+
 if __name__ == '__main__':
-    print(max_of_sum())
+    input = open('input.txt')
+    sum_l = sum_groups(input)
+    print(max(sum_l))
+
+    big_three = three_largest(sum_l)
+    print(sum(big_three))
