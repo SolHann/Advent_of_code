7d44ee09ad429e6cdd82ef909230f4636ced7786
SolHann <supersolhann@gmail.com>
2022-12-04 20:25:44 +0000

    Adds solutions to both problems for day 4

--- Day_4/main.py
+++ Day_4/main.py
@@ -1,9 +1,23 @@
 def p1(input):
-    return input
+    sum=0
+    for l in input:
+        a, c = l.split(",")
+        a, b = a.split("-")
+        c, d = c.split("-")
+        if int(a) >= int(c) and int(b) <= int(d) or int(a) <= int(c) and int(b) >= int(d):
+            sum += 1
+    return sum
 
 
 def p2(input):
-    return input
+    sum = 0
+    for l in input:
+        a, c = l.split(",")
+        a, b = a.split("-")
+        c, d = c.split("-")
+        if not (int(b) < int(c) or int(d) < int(a)):
+            sum += 1
+    return sum
 
 
 with open('input.txt', 'r') as f:
